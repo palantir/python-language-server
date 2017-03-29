469fa590a46562e95172910f28cf2e539c3afb1b
Nicholas Gates <ngates@palantir.com>
2017-03-29 16:27:09 +0100

    Add vscode example extension

---
+++ vscode-client/src/extension.ts
@@ -0,0 +1,46 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import * as net from 'net';
+
+import { workspace, Disposable, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, ErrorAction, ErrorHandler, CloseAction, TransportKind } from 'vscode-languageclient';
+
+function startLangServer(command: string, documentSelector: string | string[]): Disposable {
+	const serverOptions: ServerOptions = {
+		command: command,
+	};
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: documentSelector,
+	}
+	return new LanguageClient(command, serverOptions, clientOptions).start();
+}
+
+function startLangServerTCP(addr: number, documentSelector: string | string[]): Disposable {
+	const serverOptions: ServerOptions = function() {
+		return new Promise((resolve, reject) => {
+			var client = new net.Socket();
+			client.connect(addr, "127.0.0.1", function() {
+				resolve({
+					reader: client,
+					writer: client
+				});
+			});
+		});
+	}
+
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: documentSelector,
+	}
+	return new LanguageClient(`tcp lang server (port ${addr})`, serverOptions, clientOptions).start();
+}
+
+export function activate(context: ExtensionContext) {
+    context.subscriptions.push(startLangServer("pyls", ["python"]));
+    // For TCP
+    // context.subscriptions.push(startLangServerTCP(2087, ["pyls"]));
+}
+
