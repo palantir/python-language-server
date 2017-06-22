88712b5fe7208cf508c0467c2596663e78a9f710
Nicholas Gates <ngates@palantir.com>
2017-06-22 18:31:27 -0400

    Bump versioneer

--- pyls/__init__.py
+++ pyls/__init__.py
@@ -11,3 +11,7 @@ PYLS = 'pyls'
 
 hookspec = pluggy.HookspecMarker(PYLS)
 hookimpl = pluggy.HookimplMarker(PYLS)
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
