7b9eedbf2c77fe0ceace8d39d9ba4f09911d3a5b
Nicholas Gates <ngates@palantir.com>
2017-09-03 19:27:22 +0100

    formatting

--- pyls/python_ls.py
+++ pyls/python_ls.py
@@ -82,9 +82,7 @@ class PythonLanguageServer(LanguageServer):
 
     @_utils.debounce(LINT_DEBOUNCE_S)
     def lint(self, doc_uri):
-        self.workspace.publish_diagnostics(doc_uri, flatten(self._hook(
-            'pyls_lint', doc_uri
-        )))
+        self.workspace.publish_diagnostics(doc_uri, flatten(self._hook('pyls_lint', doc_uri)))
 
     def references(self, doc_uri, position, exclude_declaration):
         return flatten(self._hook(
