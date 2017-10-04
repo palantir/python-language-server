90e0b06273513af5c99180a652e5377d612f4979
Tom van Ommeren <tom.vanommeren@gmail.com>
2017-10-04 08:06:26 +0200

    Add tests cases for definition
    confirms no definitions returned for builtins
    (these used to return random locations in the current document)
    confirms assignment is returned instead of variable type.

--- test/plugins/test_definitions.py
+++ test/plugins/test_definitions.py
@@ -8,6 +8,14 @@ DOC = """def a():
     pass
 
 print a()
+
+
+class Directory(object):
+    def __init__(self):
+        self.members = dict()
+
+    def add_member(self, id, name):
+        self.members[id] = name
 """
 
 
@@ -19,6 +27,29 @@ def test_definitions():
     range = {
         'start': {'line': 0, 'character': 4},
         'end': {'line': 0, 'character': 5}
+    }
+
+    doc = Document(DOC_URI, DOC)
+    assert [{'uri': DOC_URI, 'range': range}] == pyls_definitions(doc, cursor_pos)
+
+
+def test_builtin_definition():
+    # Over 'i' in dict
+    cursor_pos = {'line': 8, 'character': 24}
+
+    # No go-to def for builtins
+    doc = Document(DOC_URI, DOC)
+    assert [] == pyls_definitions(doc, cursor_pos)
+
+
+def test_assignment():
+    # Over 's' in self.members[id]
+    cursor_pos = {'line': 11, 'character': 19}
+
+    # The assignment of 'self.members'
+    range = {
+        'start': {'line': 8, 'character': 13},
+        'end': {'line': 8, 'character': 20}
     }
 
     doc = Document(DOC_URI, DOC)
