b0c21abaffec2de2a8e9516018579469d6dacc4f
forozco <forozco@palantir.com>
2018-02-15 14:29:00 +0000

    py3 next

--- test/test_json_rpc_server.py
+++ test/test_json_rpc_server.py
@@ -6,7 +6,7 @@ def test_receive_request(json_rpc_server):
     client, server = json_rpc_server
     request = {'jsonrpc': '2.0', 'id': 0, 'method': 'initialize', 'params': {}}
     client.write_message(request)
-    message = server.get_messages().next()
+    message = next(server.get_messages())
     assert isinstance(message, JSONRPC20Request)
     assert request == message.data
     assert not message.is_notification
@@ -16,7 +16,7 @@ def test_receive_notification(json_rpc_server):
     client, server = json_rpc_server
     notification = {'jsonrpc': '2.0', 'method': 'notify', 'params': {}}
     client.write_message(notification)
-    message = server.get_messages().next()
+    message = next(server.get_messages())
     assert isinstance(message, JSONRPC20Request)
     assert notification == message.data
     assert message.is_notification
@@ -26,7 +26,7 @@ def test_receive_response(json_rpc_server):
     client, server = json_rpc_server
     response = {'jsonrpc': '2.0', 'id': 0, 'result': {}}
     client.write_message(response)
-    message = server.get_messages().next()
+    message = next(server.get_messages())
     assert isinstance(message, JSONRPC20Response)
     assert response == message.data
 
@@ -37,7 +37,7 @@ def test_drop_bad_message(json_rpc_server):
     client.write_message(response)
     server.close()
     try:
-        server.get_messages().next()
+        next(server.get_messages())
     except StopIteration:
         pass
     else:
