8fe2e0bc486a3294a627f30af37a364c81bceec4
Alexey Evseev <myhappydo@gmail.com>
2018-08-06 14:40:55 +0300

    Allow to use custom python interpreter [fixes #339]

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'configparser; python_version<"3.0"',
         'future>=0.14.0',
         'futures; python_version<"3.2"',
-        'jedi>=0.12',
+        'jedi>=0.13',
         'python-jsonrpc-server>=0.1.0',
         'pluggy'
     ],
