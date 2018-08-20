62fcdab578e590ec7f2e4ef3374245918e51b902
Alexey Evseev <myhappydo@gmail.com>
2018-08-20 16:04:16 +0300

    Respect option `follow_imports` in `goto_assignments` call

--- pyls/plugins/definition.py
+++ pyls/plugins/definition.py
@@ -6,8 +6,10 @@ log = logging.getLogger(__name__)
 
 
 @hookimpl
-def pyls_definitions(document, position):
-    definitions = document.jedi_script(position).goto_assignments()
+def pyls_definitions(config, document, position):
+    expected_settings = {'follow_imports', 'follow_builtin_imports'}
+    params = {k: v for k, v in config.settings().items() if k in expected_settings}
+    definitions = document.jedi_script(position).goto_assignments(**params)
 
     definitions = [
         d for d in definitions
