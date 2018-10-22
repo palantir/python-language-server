fc8c648061d0f19073577b8bac23f5f0aa49a8c9
Karel Kočí <karel.koci@nic.cz>
2018-10-22 13:00:12 +0200

    setup.py: exclude all test packages
    There is a subpackage in test. This subpackage was before this commit
    installed although test package was ignored. Extending exclude to ignore
    anything that starts with 'test' is enough to exclude any subpackage of
    test.
    
    This is causing an error when trying to package pyls in Gentoo. Gentoo
    build system disutils eclass checks if there is some test package to be
    installed and fails installation step if so. This fixes that.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'test']),
+    packages=find_packages(exclude=['contrib', 'docs', 'test', 'test.*']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
