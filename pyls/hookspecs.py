6dee330acc9bb786cf5e56c6535213c3005a6d95
Nicholas Gates <ngates@palantir.com>
2019-02-16 15:24:49 +0000

    Add workspace symbol support

--- pyls/hookspecs.py
+++ pyls/hookspecs.py
@@ -110,3 +110,8 @@ def pyls_settings(config):
 @hookspec(firstresult=True)
 def pyls_signature_help(config, workspace, document, position):
     pass
+
+
+@hookspec
+def pyls_workspace_symbols(config, workspace, query):
+    pass
---
+++ test/plugins/test_ctags.py
@@ -0,0 +1,29 @@
+# Copyright 2017 Palantir Technologies, Inc.
+import os
+import tempfile
+
+import pytest
+
+import pyls
+from pyls import lsp
+from pyls.plugins import ctags
+
+
+@pytest.fixture(scope='session')
+def pyls_ctags():
+    """Fixture for generating ctags for the Python Langyage Server"""
+    _fd, tag_file = tempfile.mkstemp()
+    try:
+        ctags.execute("ctags", tag_file, os.path.dirname(pyls.__file__))
+        yield tag_file
+    finally:
+        os.unlink(tag_file)
+
+
+def test_parse_tags(pyls_ctags):
+    # Search for CtagsPlugin with the query 'tagsplug'
+    plugin_symbol = next(ctags.parse_tags(pyls_ctags, "tagsplug"))
+
+    assert plugin_symbol['name'] == 'CtagsPlugin'
+    assert plugin_symbol['kind'] == lsp.SymbolKind.Class
+    assert plugin_symbol['location']['uri'].endswith('pyls/plugins/ctags.py')
