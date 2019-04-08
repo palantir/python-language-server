98c1f0dca0680bab399cd35320db27e6d7d15c6a
Jan Seeger <jan.seeger@siemens.com>
2019-04-08 15:48:22 +0200

    Fixed bug in Pylint config finding.
    The config key used was the string 'settings.plugins.pylint', instead
    of correct nested dictionaries.

--- pyls/config/pylint_conf.py
+++ pyls/config/pylint_conf.py
@@ -10,12 +10,25 @@ PROJECT_CONFIGS = ['.pylintrc', 'pylintrc']
 RCFILE_CONFIG = 'plugins.pylint.rcfile'
 
 
+def set_dict_path(d, path, value):
+    current = d
+    elems = path.split(".")
+    for elem in elems[:-1]:
+        if elem not in current.keys():
+            current[elem] = {}
+        current = current[elem]
+    current[elems[-1]] = value
+    return d
+
+
 class PylintConfig(ConfigSource):
     """Parse pylint configurations."""
 
     def user_config(self):
         config_file = self._user_config_file()
-        return {RCFILE_CONFIG: config_file}
+        if os.path.isfile(config_file):
+            return set_dict_path({}, RCFILE_CONFIG, config_file)
+        return {}
 
     def _user_config_file(self):
         if self.is_windows:
@@ -25,5 +38,5 @@ class PylintConfig(ConfigSource):
     def project_config(self, document_path):
         files = find_parents(self.root_path, document_path, PROJECT_CONFIGS)
         if files:
-            return {RCFILE_CONFIG: files[0]}
+            return set_dict_path({}, RCFILE_CONFIG, files[0])
         return {}
