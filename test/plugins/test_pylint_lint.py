e0bb0110ef7497de98c610f9ede9cd25234eb75a
Jan Seeger <jan.seeger@siemens.com>
2019-04-08 10:39:49 +0200

    Added test for pylint with config file.

--- test/plugins/test_pylint_lint.py
+++ test/plugins/test_pylint_lint.py
@@ -20,6 +20,11 @@ DOC_SYNTAX_ERR = """def hello()
     pass
 """
 
+PYLINTRC = """
+[MESSAGES CONTROL]
+disable=all
+"""
+
 
 @contextlib.contextmanager
 def temp_document(doc_text):
@@ -33,14 +38,25 @@ def temp_document(doc_text):
         os.remove(name)
 
 
+@contextlib.contextmanager
+def named_tempfile(path, content):
+    try:
+        f = open(path, "w")
+        f.write(content)
+        f.close()
+        yield f
+    finally:
+        os.remove(path)
+
+
 def write_temp_doc(document, contents):
     with open(document.path, 'w') as temp_file:
         temp_file.write(contents)
 
 
-def test_pylint():
+def test_pylint(config):
     with temp_document(DOC) as doc:
-        diags = pylint_lint.pyls_lint(doc, True)
+        diags = pylint_lint.pyls_lint(config, doc, True)
 
         msg = '[unused-import] Unused import sys'
         unused_import = [d for d in diags if d['message'] == msg][0]
@@ -49,9 +65,9 @@ def test_pylint():
         assert unused_import['severity'] == lsp.DiagnosticSeverity.Warning
 
 
-def test_syntax_error_pylint():
+def test_syntax_error_pylint(config):
     with temp_document(DOC_SYNTAX_ERR) as doc:
-        diag = pylint_lint.pyls_lint(doc, True)[0]
+        diag = pylint_lint.pyls_lint(config, doc, True)[0]
 
         assert diag['message'].startswith('[syntax-error] invalid syntax')
         # Pylint doesn't give column numbers for invalid syntax.
@@ -59,15 +75,15 @@ def test_syntax_error_pylint():
         assert diag['severity'] == lsp.DiagnosticSeverity.Error
 
 
-def test_lint_free_pylint():
+def test_lint_free_pylint(config):
     # Can't use temp_document because it might give us a file that doesn't
     # match pylint's naming requirements. We should be keeping this file clean
     # though, so it works for a test of an empty lint.
-    assert not pylint_lint.pyls_lint(
-        Document(uris.from_fs_path(__file__)), True)
+    assert not pylint_lint.pyls_lint(config,
+                                     Document(uris.from_fs_path(__file__)), True)
 
 
-def test_lint_caching():
+def test_lint_caching(config):
     # Pylint can only operate on files, not in-memory contents. We cache the
     # diagnostics after a run so we can continue displaying them until the file
     # is saved again.
@@ -80,25 +96,34 @@ def test_lint_caching():
     flags = '--disable=invalid-name'
     with temp_document(DOC) as doc:
         # Start with a file with errors.
-        diags = pylint_lint.PylintLinter.lint(doc, True, flags)
+        diags = pylint_lint.PylintLinter.lint(config, doc, True, flags)
         assert diags
 
         # Fix lint errors and write the changes to disk. Run the linter in the
         # in-memory mode to check the cached diagnostic behavior.
         write_temp_doc(doc, '')
-        assert pylint_lint.PylintLinter.lint(doc, False, flags) == diags
+        assert pylint_lint.PylintLinter.lint(config, doc, False, flags) == diags
 
         # Now check the on-disk behavior.
-        assert not pylint_lint.PylintLinter.lint(doc, True, flags)
+        assert not pylint_lint.PylintLinter.lint(config, doc, True, flags)
 
         # Make sure the cache was properly cleared.
-        assert not pylint_lint.PylintLinter.lint(doc, False, flags)
+        assert not pylint_lint.PylintLinter.lint(config, doc, False, flags)
 
 
-def test_per_file_caching():
+def test_per_file_caching(config):
     # Ensure that diagnostics are cached per-file.
     with temp_document(DOC) as doc:
-        assert pylint_lint.pyls_lint(doc, True)
+        assert pylint_lint.pyls_lint(config, doc, True)
 
-    assert not pylint_lint.pyls_lint(
-        Document(uris.from_fs_path(__file__)), False)
+    assert not pylint_lint.pyls_lint(config,
+                                     Document(uris.from_fs_path(__file__)), False)
+
+
+def test_with_config_file(config):
+    with temp_document(DOC) as doc:
+        with named_tempfile(os.path.join(os.path.dirname(doc.path), ".pylintrc"),
+                            PYLINTRC)\
+             as configfile:
+            config.update({'plugins': {'pylint': {'rcfile': configfile.name}}})
+            assert not pylint_lint.pyls_lint(config, doc, True)
