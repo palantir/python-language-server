528e3f0487248166a533c48a302e51358f0770c7
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2019-06-21 15:38:52 -0500

    Fix encoding errors on Py2

--- test/test_workspace.py
+++ test/test_workspace.py
@@ -19,6 +19,27 @@ def path_as_uri(path):
     return pathlib.Path(osp.abspath(path)).as_uri()
 
 
+def to_text_string(obj, encoding=None):
+    """Convert `obj` to (unicode) text string"""
+    if PY2:
+        if isinstance(obj, unicode):
+            return obj
+        # Python 2
+        if encoding is None:
+            return unicode(obj)
+        else:
+            return unicode(obj, encoding)
+    else:
+        # Python 3
+        if encoding is None:
+            return str(obj)
+        elif isinstance(obj, str):
+            # In case this function is not used properly, this could happen
+            return obj
+        else:
+            return str(obj, encoding)
+
+
 def test_local(pyls):
     """ Since the workspace points to the test directory """
     assert pyls.workspace.is_local()
@@ -73,7 +94,7 @@ def test_multiple_workspaces(tmpdir, pyls):
     file2.write('import sys')
 
     msg = {
-        'uri': path_as_uri(str(file1)),
+        'uri': to_text_string(path_as_uri(to_text_string(file1))),
         'version': 1,
         'text': 'import os'
     }
@@ -81,7 +102,7 @@ def test_multiple_workspaces(tmpdir, pyls):
     pyls.m_text_document__did_open(textDocument=msg)
     assert msg['uri'] in pyls.workspace._docs
 
-    added_workspaces = [{'uri': path_as_uri(str(x))}
+    added_workspaces = [{'uri': to_text_string(path_as_uri(to_text_string(x)))}
                         for x in (workspace1_dir, workspace2_dir)]
     pyls.m_workspace__did_change_workspace_folders(
         added=added_workspaces, removed=[])
@@ -94,7 +115,7 @@ def test_multiple_workspaces(tmpdir, pyls):
     assert msg['uri'] in pyls.workspaces[workspace1_uri]._docs
 
     msg = {
-        'uri': path_as_uri(str(file2)),
+        'uri': to_text_string(path_as_uri(to_text_string(file2))),
         'version': 1,
         'text': 'import sys'
     }
