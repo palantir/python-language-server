85b2322329f8db4b471c582263ccd618581ed684
mpanarin <mykhailopanarin@gmail.com>
2019-07-17 20:33:47 +0300

    Update pyls hover to specify language of signature.
    LSP supports `MarkedString[]` to specify render engine. We can definitely
    say that function signature should be rendered as python. For the docstring,
    as we have no good way of parsing it to guess the engine, we can pass it
    as markdown.

--- pyls/plugins/hover.py
+++ pyls/plugins/hover.py
@@ -10,9 +10,26 @@ def pyls_hover(document, position):
     definitions = document.jedi_script(position).goto_definitions()
     word = document.word_at_position(position)
 
-    # Find an exact match for a completion
-    for d in definitions:
-        if d.name == word:
-            return {'contents': _utils.format_docstring(d.docstring()) or ''}
+    # Find first exact matching definition
+    definition = next((x for x in definitions if x.name == word), None)
 
-    return {'contents': ''}
+    if not definition:
+        return {'contents': ''}
+
+    # raw docstring returns only doc, without signature
+    doc = _utils.format_docstring(definition.docstring(raw=True))
+
+    # Find first exact matching signature
+    signature = next((x.to_string() for x in definition.get_signatures() if x.name == word), '')
+
+    contents = []
+    if signature:
+        contents.append({
+            'language': 'python',
+            'value': signature,
+        })
+    if doc:
+        contents.append(doc)
+    if not contents:
+        return {'contents': ''}
+    return {'contents': contents}
