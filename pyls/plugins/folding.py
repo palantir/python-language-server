e0a1207792398203f7ae6a935fc6bf006f7d0d95
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2019-09-30 14:47:56 -0500

    Reduce code complexity

--- pyls/plugins/folding.py
+++ pyls/plugins/folding.py
@@ -3,6 +3,73 @@ import ast
 from pyls import hookimpl
 
 
+def __update_folding_ranges(ctx, stack, line, col, folding_starts,
+                            folding_ranges):
+    _, next_ctx = stack[0]
+    while ctx != next_ctx:
+        this_ctx = ctx.pop(0)
+        if this_ctx in folding_starts:
+            ctx_start = folding_starts.pop(this_ctx)
+            ctx_line, _ = ctx_start
+            if ctx_line < line:
+                folding_ranges.append((ctx_start, (line, col)))
+    return folding_ranges
+
+
+def __reduce_folding_ranges(folding_ranges):
+    actual_ranges = []
+    i = 0
+    while i < len(folding_ranges) - 1:
+        ((left_line, left_column), left_end) = folding_ranges[i]
+        if i + 1 > len(folding_ranges):
+            actual_ranges.append(folding_ranges[i])
+        else:
+            ((right_line, right_column), right_end) = folding_ranges[i + 1]
+            left_end_line, _ = left_end
+            right_end_line, _ = right_end
+            if left_line == right_line and left_end == right_end:
+                actual_ranges.append(folding_ranges[i + 1])
+                i += 2
+            elif left_line == right_line and left_end_line >= right_end_line:
+                actual_ranges.append(folding_ranges[i])
+                i += 2
+            elif left_line == right_line and left_end_line < right_end_line:
+                actual_ranges.append(folding_ranges[i + 1])
+                i += 2
+            else:
+                actual_ranges.append(folding_ranges[i])
+                i += 1
+    return actual_ranges
+
+
+def __compute_folding_ranges(tree):
+    folding_ranges = []
+    folding_starts = {}
+
+    stack = [(tree, [])]
+    while len(stack) > 0:
+        node, ctx = stack.pop(0)
+        node_id = id(node)
+        if hasattr(node, 'lineno'):
+            line, col = node.lineno, node.col_offset
+            folding_starts[node_id] = (line, col)
+
+        nodes = []
+        new_ctx = [node_id] + ctx
+        for child in ast.iter_child_nodes(node):
+            nodes.append((child, new_ctx))
+        stack = nodes + stack
+
+        if len(nodes) == 0:
+            if len(stack) > 0:
+                folding_ranges = __update_folding_ranges(
+                    ctx, stack, line, col, folding_starts, folding_ranges)
+
+    folding_ranges = sorted(folding_ranges)
+    ranges = __reduce_folding_ranges(folding_ranges)
+    return ranges
+
+
 @hookimpl
 def pyls_folding_range(document):
     program = str(document.source)
@@ -26,60 +93,10 @@ def pyls_folding_range(document):
         program = program + '\n\npass'
         tree = ast.parse(program)
 
-    folding_ranges = []
-    folding_starts = {}
-
-    stack = [(tree, [])]
-    while len(stack) > 0:
-        node, ctx = stack.pop(0)
-        node_id = id(node)
-        if hasattr(node, 'lineno'):
-            line, col = node.lineno, node.col_offset
-            folding_starts[node_id] = (line, col)
-
-        nodes = []
-        new_ctx = [node_id] + ctx
-        for child in ast.iter_child_nodes(node):
-            nodes.append((child, new_ctx))
-        stack = nodes + stack
-
-        if len(nodes) == 0:
-            if len(stack) > 0:
-                _, next_ctx = stack[0]
-                while ctx != next_ctx:
-                    this_ctx = ctx.pop(0)
-                    if this_ctx in folding_starts:
-                        ctx_start = folding_starts.pop(this_ctx)
-                        ctx_line, _ = ctx_start
-                        if ctx_line < line:
-                            folding_ranges.append((ctx_start, (line, col)))
-
-    folding_ranges = sorted(folding_ranges)
-    actual_ranges = []
-    i = 0
-    while i < len(folding_ranges) - 1:
-        ((left_line, left_column), left_end) = folding_ranges[i]
-        if i + 1 > len(folding_ranges):
-            actual_ranges.append(folding_ranges[i])
-        else:
-            ((right_line, right_column), right_end) = folding_ranges[i + 1]
-            left_end_line, _ = left_end
-            right_end_line, _ = right_end
-            if left_line == right_line and left_end == right_end:
-                actual_ranges.append(folding_ranges[i + 1])
-                i += 2
-            elif left_line == right_line and left_end_line >= right_end_line:
-                actual_ranges.append(folding_ranges[i])
-                i += 2
-            elif left_line == right_line and left_end_line < right_end_line:
-                actual_ranges.append(folding_ranges[i + 1])
-                i += 2
-            else:
-                actual_ranges.append(folding_ranges[i])
-                i += 1
+    ranges = __compute_folding_ranges(tree)
 
     results = []
-    for ((start_line, _), (end_line, _)) in actual_ranges:
+    for ((start_line, _), (end_line, _)) in ranges:
         start_line -= 1
         end_line -= 1
         # If start/end character is not defined, then it defaults to the
