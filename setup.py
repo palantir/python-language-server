f619d3a26fee801fa96b77072535a8a020d0c685
Carlos Cordoba <ccordoba12@gmail.com>
2019-10-26 20:39:27 -0500

    Add completion tests for several libraries of the Scientific Python stack (#682)

--- setup.py
+++ setup.py
@@ -65,7 +65,8 @@ setup(
         'pylint': ['pylint'],
         'rope': ['rope>0.10.5'],
         'yapf': ['yapf'],
-        'test': ['versioneer', 'pylint', 'pytest', 'mock', 'pytest-cov', 'coverage'],
+        'test': ['versioneer', 'pylint', 'pytest', 'mock', 'pytest-cov',
+                 'coverage', 'numpy', 'pandas', 'matplotlib'],
     },
 
     # To provide executable scripts, use entry points in preference to the
