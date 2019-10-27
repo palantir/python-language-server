5fa5cd68c7fd51dd22dcd078e68d718e4eb6f873
Carlos Cordoba <ccordoba12@gmail.com>
2019-10-27 18:30:49 -0500

    Declare the minimal Pylint version required for our tests to pass

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
             'pycodestyle',
             'pydocstyle>=2.0.0',
             'pyflakes>=1.6.0',
-            'pylint',
+            'pylint>=2.4.0',
             'rope>=0.10.5',
             'yapf',
         ],
