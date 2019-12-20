02c000e872c140dd6f12c367880737c1bf4d3d01
John Ayad <johnhany97@gmail.com>
2019-12-20 22:38:45 +0000

    Remove print statement

--- test/plugins/test_pycodestyle_lint.py
+++ test/plugins/test_pycodestyle_lint.py
@@ -23,7 +23,7 @@ import json
 def test_pycodestyle(config):
     doc = Document(DOC_URI, DOC)
     diags = pycodestyle_lint.pyls_lint(config, doc)
-    print("diagasoibfaosi", diags)
+
     assert all([d['source'] == 'pycodestyle' for d in diags])
 
     # One we're expecting is:
