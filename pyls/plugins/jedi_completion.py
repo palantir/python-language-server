e20bf30d9739194c4f1599ec0c44a39397f1b1fb
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-03-17 22:07:22 -0500

    Escape Jedi path completions (#762)

--- pyls/plugins/jedi_completion.py
+++ pyls/plugins/jedi_completion.py
@@ -1,5 +1,6 @@
 # Copyright 2017 Palantir Technologies, Inc.
 import logging
+import os.path as osp
 import parso
 from pyls import hookimpl, lsp, _utils
 
@@ -22,6 +23,7 @@ _TYPE_MAP = {
     'builtinfunction': lsp.CompletionItemKind.Function,
     'module': lsp.CompletionItemKind.Module,
     'file': lsp.CompletionItemKind.File,
+    'path': lsp.CompletionItemKind.Text,
     'xrange': lsp.CompletionItemKind.Class,
     'slice': lsp.CompletionItemKind.Class,
     'traceback': lsp.CompletionItemKind.Class,
@@ -129,6 +131,12 @@ def _format_completion(d, include_params=True):
         'sortText': _sort_text(d),
         'insertText': d.name
     }
+
+    if d.type == 'path':
+        path = osp.normpath(d.name)
+        path = path.replace('\\', '\\\\')
+        path = path.replace('/', '\\/')
+        completion['insertText'] = path
 
     if (include_params and hasattr(d, 'params') and d.params and
             not is_exception_class(d.name)):
