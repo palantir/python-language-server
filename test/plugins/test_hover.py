216b01ff7bd0845ead7a031972ece06d1ca962aa
dalthviz <d.althviz10@uniandes.edu.co>
2020-03-27 13:18:58 -0500

    Fix hover request for numpy alias (np) and ufuncs

--- test/plugins/test_hover.py
+++ test/plugins/test_hover.py
@@ -13,6 +13,51 @@ def main():
     pass
 """
 
+NUMPY_DOC = """
+
+import numpy as np
+np.sin
+
+"""
+
+def test_numpy_hover():
+    # Over the blank line
+    no_hov_position = {'line': 1, 'character': 0}
+    # Over 'numpy' in import numpy as np
+    numpy_hov_position_1 = {'line': 2, 'character': 8}
+    # Over 'np' in import numpy as np
+    numpy_hov_position_2 = {'line': 2, 'character': 17}
+    # Over 'np' in np.sin
+    numpy_hov_position_3 = {'line': 3, 'character': 1}
+    # Over 'sin' in np.sin
+    numpy_sin_hov_position = {'line': 3, 'character': 4}
+
+    doc = Document(DOC_URI, NUMPY_DOC)
+
+    if LooseVersion(_utils.JEDI_VERSION) >= LooseVersion('0.15.0'):
+        contents = [
+            {'language': 'python', 'value': 'numpy'},
+            'NumPy\n=====\n\nProvides\n\xa0\xa01. '
+            'An array object of arbitrary homogeneous items\n\xa0\xa02.']
+        assert contents in pyls_hover(doc, numpy_hov_position_1)['contents']
+
+        contents = [
+            {'language': 'python', 'value': 'numpy'},
+            'NumPy\n=====\n\nProvides\n\xa0\xa01. '
+            'An array object of arbitrary homogeneous items\n\xa0\xa02.']
+        assert contents in pyls_hover(doc, numpy_hov_position_2)['contents']
+
+        contents = [
+            {'language': 'python', 'value': 'numpy'},
+            'NumPy\n=====\n\nProvides\n\xa0\xa01. '
+            'An array object of arbitrary homogeneous items\n\xa0\xa02.']
+        assert contents in pyls_hover(doc, numpy_hov_position_3)['contents']
+
+        contents = [
+            {'language': 'python', 'value': 'numpy'},
+            'Trigonometric sine, element-wise.\n\n']
+        assert contents in pyls_hover(doc, numpy_sin_hov_position)['contents']
+
 
 def test_hover():
     # Over 'main' in def main():
