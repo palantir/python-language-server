c5f590e5325c59db227cc3491993cc49b36021cb
goanpeca <goanpeca@gmail.com>
2020-05-12 11:10:27 -0500

    Update ujson dependency

--- setup.py
+++ setup.py
@@ -36,9 +36,9 @@ setup(
         'future>=0.14.0; python_version<"3"',
         'backports.functools_lru_cache; python_version<"3.2"',
         'jedi>=0.17.0,<0.18.0',
-        'python-jsonrpc-server>=0.3.2',
+        'python-jsonrpc-server>=0.3.3',
         'pluggy',
-        'ujson<=1.35; platform_system!="Windows"'
+        'ujson>=2'
     ],
 
     # List additional groups of dependencies here (e.g. development
