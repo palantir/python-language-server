d62490faaa432344bd33c7c282b47a45373c48dd
Ryan Clary <rclary@trialphaenergy.com>
2020-06-20 16:50:44 -0700

    * Allow explicit environment variables passed to jedi environment (see davidhalter/jedi#1617 and davidhalter/jedi#1619) * Requires jedi>=0.17.2

--- pyls/workspace.py
+++ pyls/workspace.py
@@ -226,14 +226,16 @@ class Document(object):
     def jedi_script(self, position=None):
         extra_paths = []
         environment_path = None
+        env_vars = {}
 
         if self._config:
             jedi_settings = self._config.plugin_settings('jedi', document_path=self.path)
             environment_path = jedi_settings.get('environment')
             extra_paths = jedi_settings.get('extra_paths') or []
+            env_vars = jedi_settings.get('env_vars') or {}
 
-        environment = self.get_enviroment(environment_path) if environment_path else None
-        sys_path = self.sys_path(environment_path) + extra_paths
+        environment = self.get_enviroment(environment_path, env_vars=env_vars) if environment_path else None
+        sys_path = self.sys_path(environment_path, env_vars=env_vars) + extra_paths
         project_path = self._workspace.root_path
 
         kwargs = {
@@ -249,7 +251,8 @@ class Document(object):
 
         return jedi.Script(**kwargs)
 
-    def get_enviroment(self, environment_path=None):
+    def get_enviroment(self, environment_path=None, **kwargs):
+        # TODO: when safe to break API, use env_vars explicitly to pass to create_environment
         # TODO(gatesn): #339 - make better use of jedi environments, they seem pretty powerful
         if environment_path is None:
             environment = jedi.api.environment.get_cached_default_environment()
@@ -257,14 +260,16 @@ class Document(object):
             if environment_path in self._workspace._environments:
                 environment = self._workspace._environments[environment_path]
             else:
-                environment = jedi.api.environment.create_environment(path=environment_path, safe=False)
+                environment = jedi.api.environment.create_environment(path=environment_path,
+                                                                      safe=False, **kwargs)
                 self._workspace._environments[environment_path] = environment
 
         return environment
 
-    def sys_path(self, environment_path=None):
+    def sys_path(self, environment_path=None, **kwargs):
         # Copy our extra sys path
+        # TODO: when safe to break API, use env_vars explicitly to pass to create_environment
         path = list(self._extra_sys_path)
-        environment = self.get_enviroment(environment_path=environment_path)
+        environment = self.get_enviroment(environment_path=environment_path, **kwargs)
         path.extend(environment.get_sys_path())
         return path
