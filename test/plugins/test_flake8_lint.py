f50af9cc41abba077870af37bad136312907588d
Carlos Cordoba <ccordoba12@gmail.com>
2020-06-30 17:07:43 -0500

    Fix test_flake8_executable_param

--- test/plugins/test_flake8_lint.py
+++ test/plugins/test_flake8_lint.py
@@ -67,16 +67,16 @@ def test_flake8_config_param(workspace):
         assert '--config={}'.format(flake8_conf) in call_args
 
 
-def test_flake8_executable_param(config):
+def test_flake8_executable_param(workspace):
     with patch('pyls.plugins.flake8_lint.Popen') as popen_mock:
         mock_instance = popen_mock.return_value
         mock_instance.communicate.return_value = [bytes(), bytes()]
 
         flake8_executable = '/tmp/flake8'
-        config.update({'plugins': {'flake8': {'executable': flake8_executable}}})
+        workspace._config.update({'plugins': {'flake8': {'executable': flake8_executable}}})
 
-        _name, doc = temp_document(DOC)
-        flake8_lint.pyls_lint(config, doc)
+        _name, doc = temp_document(DOC, workspace)
+        flake8_lint.pyls_lint(workspace, doc)
 
         call_args = popen_mock.call_args.args[0]
         assert flake8_executable in call_args
