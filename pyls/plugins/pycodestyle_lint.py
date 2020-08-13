a25c95cebcdf62f8dedb7e8ce25dae6d5d3f22ff
Ben Ramsden <bramsden@palantir.com>
2020-08-13 18:26:47 +0100

    indent

--- pyls/plugins/pycodestyle_lint.py
+++ pyls/plugins/pycodestyle_lint.py
@@ -73,5 +73,5 @@ def _get_severity(code):
     if code[0] == 'E' or code[0] == 'W':
         return lsp.DiagnosticSeverity.Warning
     else:
-    # If no severity is specified, why wouldn't this be informational only?
+        # If no severity is specified, why wouldn't this be informational only?
         return lsp.DiagnosticSeverity.Info
