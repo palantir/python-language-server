36da0249b0ba6438416c335c31c6ce16e05e3486
Daniel Zvara <dan.zvara@gmail.com>
2020-10-18 12:33:05 +0200

    Add jedi lint plugin

---
+++ pyls/plugins/jedi_lint.py
@@ -0,0 +1,28 @@
+from pyls import hookimpl, lsp
+
+@hookimpl
+def pyls_lint(config, document):
+    errors = document.jedi_script().get_syntax_errors()
+    diagnostics = []
+
+    for error in errors:
+        err_range = {
+            'start': {
+              'line': error.line - 1,
+               # Index columns start from 0
+               'character': error.column,
+            },
+            'end': {
+                'line': error.until_line - 1,
+                # It's possible that we're linting an empty file. Even an empty
+                # file might fail linting if it isn't named properly.
+                'character': error.until_column,
+                },
+            }
+        diagnostics.append({
+                'source': 'jedi',
+                'range': err_range,
+                'message': error.get_message(),
+                'severity': lsp.DiagnosticSeverity.Error,
+            })
+    return diagnostics
