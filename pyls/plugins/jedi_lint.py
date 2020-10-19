8858b067c713b72c598db8ab1093fa095a6afbbd
Daniel Zvara <dan.zvara@gmail.com>
2020-10-19 12:00:50 +0200

    Fix pycodestyle errors

--- pyls/plugins/jedi_lint.py
+++ pyls/plugins/jedi_lint.py
@@ -11,14 +11,14 @@ def pyls_lint(document):
             'start': {
                 'line': error.line - 1,
                 'character': error.column,
-                },
+            },
             'end': {
                 'line': error.until_line - 1,
                 # It's possible that we're linting an empty file. Even an empty
                 # file might fail linting if it isn't named properly.
                 'character': error.until_column,
-                },
-            }
+            },
+        }
         diagnostics.append({
             'source': 'jedi',
             'range': err_range,
