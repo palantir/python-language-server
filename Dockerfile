fc8c9e07be17f386258efe611ee81716e83eb4fc
Syps <nsypteras@gmail.com>
2021-05-01 15:38:05 +0800

    move lang server to github workflow

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.6.13-buster
+
+RUN pip install tornado pyflakes jedi requests beautifulsoup4
+RUN pip install -U python-jsonrpc-server
+RUN pip install git+https://github.com/NapkinHQ/python-language-server.git
+
+RUN apt-get update && apt-get install -y nginx lsof
+
+# clone and install pyls fork
+WORKDIR /napkin-ls
+
+RUN ulimit -n 500000
+#ADD server.py .
+ADD server_test.py ./server.py
+ADD nginx_default /etc/nginx/sites-available/default
+
+
+ENTRYPOINT ["/bin/bash", "-c", "nginx; ./server.py"]
---
+++ server_test.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import logging
+
+from tornado import web, ioloop, websocket, process
+from tornado.platform.asyncio import AnyThreadEventLoopPolicy
+
+from pyls_jsonrpc import dispatchers, endpoint
+from pyls.python_ls import PythonLanguageServer
+from io import BytesIO
+import threading
+import asyncio
+try:
+    import ujson as json
+except Exception:  # pylint: disable=broad-except
+    import json
+
+log = logging.getLogger(__name__)
+
+
+class LanguageServerWebSocketHandler(websocket.WebSocketHandler):
+    """Setup tornado websocket handler to host language server."""
+
+    def __init__(self, *args, **kwargs):
+        # Create an instance of the language server used to dispatch JSON RPC methods
+        self.rx = BytesIO()
+        self.tx = BytesIO()
+        langserver = PythonLanguageServer(self.rx, self.tx)
+
+        def write_a_message(msg):
+            self.write_message(json.dumps(msg))
+
+        # Setup an endpoint that dispatches to the ls, and writes server->client messages
+        # back to the client websocket
+        self.endpoint = endpoint.Endpoint(langserver, write_a_message)
+
+        # Give the language server a handle to the endpoint so it can send JSON RPC
+        # notifications and requests.
+        langserver._endpoint = self.endpoint
+
+        super(LanguageServerWebSocketHandler, self).__init__(*args, **kwargs)
+
+        langserver.start()
+
+    def on_message(self, message):
+        """Forward client->server messages to the endpoint."""
+        self.endpoint.consume(json.loads(message))
+
+
+
+    def check_origin(self, origin):
+        return True
+
+
+class HealthCheckHandler(web.RequestHandler):
+
+    def get(self):
+        self.write("OK")
+
+if __name__ == '__main__':
+    asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
+    app = web.Application([
+        (r"/python", LanguageServerWebSocketHandler),
+        (r"/health", HealthCheckHandler),
+        (r"/", HealthCheckHandler),
+    ])
+    app.listen(3002)
+    ioloop.IOLoop.current().start()
