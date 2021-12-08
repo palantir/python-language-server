c6e1b75e35cc50d83e9fb2a0ab01443b603ff5cf
masfrost <farismasad@gmail.com>
2021-12-07 20:20:39 -0800

    Prefer yapf

--- pyls/plugins/autopep8_format.py
+++ pyls/plugins/autopep8_format.py
@@ -7,13 +7,13 @@ from pyls import hookimpl
 log = logging.getLogger(__name__)
 
 
-@hookimpl(tryfirst=True)  # Prefer autopep8 over YAPF
+@hookimpl
 def pyls_format_document(config, document):
     log.info("Formatting document %s with autopep8", document)
     return _format(config, document)
 
 
-@hookimpl(tryfirst=True)  # Prefer autopep8 over YAPF
+@hookimpl
 def pyls_format_range(config, document, range):  # pylint: disable=redefined-builtin
     log.info("Formatting document %s in range %s with autopep8", document, range)
 
